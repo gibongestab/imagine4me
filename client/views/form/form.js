f8a1f5af33bc071497a394627dbf16c03961efd0
gibongestab <giselleblucas@gmail.com>
2016-05-13 19:50:39 -0300

    letras movendo

--- client/views/form/form.js
+++ client/views/form/form.js
@@ -59,7 +59,7 @@
 Template.form.events({
   'submit #contact-form'(event, template) {
     event.preventDefault();
-    
+
     console.log("form submit");
 
     let nameInput = $('#form-name');
@@ -78,13 +78,30 @@ Template.form.events({
         text
       }
 
-      console.log(req);
+
+      let errorMsg = TAPi18n.__('error');
+      let loadingMsg = TAPi18n.__('loading');
+      let successMsg = TAPi18n.__('success');
+      let submitMsg = TAPi18n.__('contact_submit');
+      
+      $('#form-send').text(loadingMsg);
+      nameInput.val('');
+      emailInput.val('');
+      textInput.val('');
+
       Meteor.call('contact', req, (err, res) => {
         if (err) {
           // houve algum erro na requisicao
+          $("#form-send").text(errorMsg);
+          $('#form-send').addClass('btn-danger');          
         } else {
           // tudo ok
+          $("#form-send").text(successMsg);
+          $('#form-send').addClass('btn-success');
         }
+        setTimeout(function() {
+          $('#form-send').text(submitMsg).removeClass('btn-danger').removeClass('btn-success');
+        }, 3000);        
       })
     } else {
       // o cara nao preencheu tudo
--- server/server.js
+++ server/server.js
@@ -5,23 +5,25 @@ Meteor.methods({
 		check(req.email, String);
 		check(req.text, String);
 
-		let fromName = "Imagine4 Contact" //aqui eh de quem voce vai receber o email (nome)
+		let from = "contact@imagine4me.com" //aqui eh de quem voce vai receber o email (nome)
 		let to = "contact@imagine4me.com" //aqui eh pra onde o email vai ser disparado
 		let subject = `Someone made contact - ${req.name}`  //Assunto do email, ai ter o nome da pessoa
-		let text = `
-			<ul>
-				<li><b>Name</b>: ${req.name}</li>
-				<li><b>Email</b>: ${req.email}</li>
-				<li><b>Text</b>: ${req.text}</li>
-			</ul>
+		let html = `
+		<ul>
+			<li><b>Name</b>: ${req.name}</li>
+			<li><b>Email</b>: ${req.email}</li>
+			<li><b>Text</b>: ${req.text}</li>
+		</ul>
 
-			Hope you get this client (:
-		`;
-		Email.send({
-			from: fromName,
+		Hope you get this client (:`;
+
+		let obj = {
+			from,
 			to,
 			subject,
-			text
-		})
+			html
+		}
+		
+		return Email.send(obj)
 	}
 })
